7eb5d7a609ac648202f06ee40d8124a2ada24d28
beazlr02 <ross.beazley@bbc.co.uk>
2020-11-05 08:53:21 +0000

    Perform big tidy up of MultiThreadedExtractorDecodingActivity.kt

--- app/src/main/java/uk/co/rossbeazley/mediacodec/MultiThreadedExtractorDecodingActivity.kt
+++ app/src/main/java/uk/co/rossbeazley/mediacodec/MultiThreadedExtractorDecodingActivity.kt
@@ -1,7 +1,6 @@
 package uk.co.rossbeazley.mediacodec
 
 import android.app.Activity
-import android.content.res.AssetFileDescriptor
 import android.media.MediaCodec
 import android.media.MediaCodecInfo
 import android.media.MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible
@@ -13,10 +12,10 @@ import android.util.Log
 import android.view.Surface
 import android.view.SurfaceHolder
 import android.view.SurfaceView
-import java.nio.ByteBuffer
 
 class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callback {
 
+    lateinit var extracter: VideoM4SExtractor
     private lateinit var initExtractor: InitM4SExtractor
     var NOT_DONE = true
 
@@ -28,30 +27,62 @@ class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callbac
         givenASegmentIsLoadedIntoMemory()
     }
 
-    lateinit var bytes: ByteArray
+    fun givenASegmentIsLoadedIntoMemory() {
+        val assetFileDescriptor = getAssets().openFd("seg1.m4s")
+        val bytes: ByteArray = assetFileDescriptor.createInputStream().use { it.readBytes() }
+        extracter = VideoM4SExtractor(bytes)
 
-    private fun readInitialisationData(assetFileDescriptor: AssetFileDescriptor): MediaFormat {
-        bytes = assetFileDescriptor.createInputStream().use { it.readBytes() }
-
-
-        val mediaFormat = MediaFormat.createVideoFormat("video/avc", 192,108)
-        val CSD0 = ByteBuffer.wrap( bytes.sliceArray(0 until 27))
-        val csd1 = ByteBuffer.wrap(bytes.sliceArray(27 until 31))
-        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, COLOR_FormatYUV420Flexible)
-        //mediaFormat.setByteBuffer("csd-0", CSD0)
-        //mediaFormat.setByteBuffer("csd-1", csd1)
-        return mediaFormat
+        initExtractor = InitM4SExtractor(getAssets().openFd("init.mp4").createInputStream().use { it.readBytes() })
     }
 
-    private fun feedCodecWithMedia(assetFileDescriptor: AssetFileDescriptor, decoder: MediaCodec) {
+    private fun startPlayback(toSurface: Surface) {
+        logMain("Start Playback")
+        val decoder = cobbleTogetherACodec(toSurface)
+        //thread 1
+        feedCodecWithMedia(extracter, decoder)
+        //thread 2
+        renderToScreen(decoder, toSurface)
+    }
+
+    private fun cobbleTogetherACodec(surface : Surface): MediaCodec {
+
+        logInput("cobbleTogetherACodec")
+
+        //val decoder = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
+        logInput("CREATED")
+        val format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, 192, 108)
+        format.setLong(MediaFormat.KEY_DURATION, 96000)
+        format.setInteger(MediaFormat.KEY_CAPTURE_RATE, 25)
+        format.setInteger(MediaFormat.KEY_PROFILE, MediaCodecInfo.CodecProfileLevel.AVCProfileConstrainedBaseline)
+        format.setInteger(MediaFormat.KEY_LEVEL, MediaCodecInfo.CodecProfileLevel.AV1Level21)
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, 25)
+        format.setInteger(MediaFormat.KEY_WIDTH, 192)
+        format.setInteger(MediaFormat.KEY_HEIGHT, 108)
+        format.setInteger(MediaFormat.KEY_MAX_WIDTH, 1920)
+        format.setInteger(MediaFormat.KEY_MAX_HEIGHT, 1080)
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, COLOR_FormatYUV420Flexible)
+        format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 16128)
+        val decoder = MediaCodec.createByCodecName( MediaCodecList(MediaCodecList.REGULAR_CODECS).findDecoderForFormat(format) )
+        //need to work out the format proper like
+        decoder.configure(format, surface, null, 0)
+
+        logInput("CONFIGURED")
+        decoder.start()
+        logInput("STARTED")
+
+        SystemClock.sleep(1000)
+        return decoder
+    }
+
+
+    private fun feedCodecWithMedia(extracter: VideoM4SExtractor, decoder: MediaCodec) {
 
        Thread {
 
            val frameCount = extracter.frameCount
-           for(frame in 0 until frameCount) {
-               feedCodec(decoder, frame)
+           for(frame in 0 until 8) {
+               feedCodec(extracter, decoder, frame)
            }
-
 
            val inIndex = decoder.dequeueInputBuffer(10000)
            if (inIndex >= 0) {
@@ -60,7 +91,6 @@ class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callbac
                //its the end of media
                logInput("Got EOF")
                decoder.queueInputBuffer(inIndex, 0, 0, System.nanoTime(), MediaCodec.BUFFER_FLAG_END_OF_STREAM)
-               //NOT_DONE = false
            } else {
                logInput("No buffers for EOS")
            }
@@ -69,16 +99,33 @@ class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callbac
 
     }
 
+    private fun feedCodec(extracter: VideoM4SExtractor, decoder: MediaCodec, frameIdx: Int) {
+        logInput("Feeding           ===== ${frameIdx}")
+        val sample = extracter.sample(frameIdx)
+        val inIndex = decoder.dequeueInputBuffer(10000)
+        logInput("inIndex ${inIndex}")
+        val buffer = decoder.getInputBuffer(inIndex)!!
+        buffer.clear()
+        val avccToAnnexB = extracter.avccToAnnexB(sample)
+        buffer.put(sample)
+        val sampleSize: Int = buffer.position()
+        logInput("sample size ${sampleSize}")
+        val flags = when (frameIdx) {
+            0 -> 0.or(MediaCodec.BUFFER_FLAG_CODEC_CONFIG).or(MediaCodec.BUFFER_FLAG_KEY_FRAME)
+            else -> 0
+        }
+        buffer.flip()
+        decoder.queueInputBuffer(inIndex, frameIdx, sampleSize, System.nanoTime(), flags)
+        logInput("samqueueInputBuffer ${flags}")
+    }
+
     private fun renderToScreen(decoder: MediaCodec, surface: Surface) {
 
         Thread() {
-            //decoder.setOutputSurface(surface)
             val startTime = System.nanoTime()
-
             val info = MediaCodec.BufferInfo()
 
             while (NOT_DONE) {
-                //logOutput("dequeueOutputBuffer ${decoder.outputFormat}")
                 val outIndex = decoder.dequeueOutputBuffer(info, 10000)
                 when (outIndex) {
                     MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED -> logOutput("INFO_OUTPUT_BUFFERS_CHANGED")
@@ -102,8 +149,6 @@ class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callbac
 
 
                 // All decoded frames have been rendered, we can stop playing now
-
-
                 // All decoded frames have been rendered, we can stop playing now
                 if (info.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM != 0) {
                     logOutput("OutputBuffer BUFFER_FLAG_END_OF_STREAM")
@@ -116,113 +161,12 @@ class MultiThreadedExtractorDecodingActivity : Activity(), SurfaceHolder.Callbac
         }.start()
     }
 
+    override fun surfaceCreated(holder: SurfaceHolder) = startPlayback(holder.surface)
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
-
-    override fun surfaceCreated(holder: SurfaceHolder) {
-
-        logMain("surface created")
-
-        val surface = holder.surface
-        val decoder = cobbleTogetherACodec(surface)
-        //        bitmapDecoded(decoder)
-        //thread 1
-        feedCodecWithMedia(getAssets().openFd("seg1.m4s"), decoder)
-        //thread 2
-        renderToScreen(decoder, holder.surface)
-    }
     override fun surfaceDestroyed(holder: SurfaceHolder) {}
 
+    private fun logMain(message: String) = Log.d("DecodeActivityInput", message)
+    private fun logInput(message: String) =Log.d("DecodeActivityInput", message)
+    private fun logOutput(message: String) =Log.d("DecodeActivityOutput", message)
 
-    private fun logMain(message: String) {
-        Log.d("DecodeActivityInput", message)
-    }
-
-    private fun logInput(message: String) {
-        Log.d("DecodeActivityInput", message)
-    }
-
-    private fun logOutput(message: String) {
-        Log.d("DecodeActivityOutput", message)
-    }
-
-
-    lateinit var extracter: VideoM4SExtractor
-
-    fun givenASegmentIsLoadedIntoMemory() {
-        val assetFileDescriptor = getAssets().openFd("seg1.m4s")
-        val bytes: ByteArray = assetFileDescriptor.createInputStream().use { it.readBytes() }
-        extracter = VideoM4SExtractor(bytes)
-
-        initExtractor = InitM4SExtractor(getAssets().openFd("init.mp4").createInputStream().use { it.readBytes() })
-    }
-
-    private fun feedCodec(decoder: MediaCodec, i: Int) {
-        logInput("Feeding ${i}")
-        val sample = extracter.sample(i)
-
-        feedByteArray(decoder, sample, i)
-    }
-
-    private fun feedByteArray(decoder: MediaCodec, sample: ByteArray, i: Int) {
-        val inIndex = decoder.dequeueInputBuffer(10000)
-        logInput("inIndex ${inIndex}")
-        val buffer = decoder.getInputBuffer(inIndex)!!
-//
-//        if(i==0) {
-//            val payload = initExtractor.avcCBox().payload
-//            buffer.put(0x0.toByte())
-//            buffer.put(0x0.toByte())
-//            buffer.put(0x0.toByte())
-//            buffer.put(0x1.toByte())
-//            buffer.put(payload)
-//
-//        }
-        val avccToAnnexB = extracter.avccToAnnexB(sample)
-        buffer.put(avccToAnnexB)
-
-
-
-        val sampleSize: Int = buffer.position()
-        logInput("sample size ${sampleSize}")
-        //BUFFER_FLAG_KEY_FRAME | BUFFER_FLAG_CODEC_CONFIG
-        val flags = when (i) {
-            0 -> 0.or(MediaCodec.BUFFER_FLAG_CODEC_CONFIG).or(MediaCodec.BUFFER_FLAG_KEY_FRAME)
-            else -> 0
-        }
-        buffer.position(0)
-        decoder.queueInputBuffer(inIndex, i, sampleSize, System.nanoTime(), flags)
-        logInput("samqueueInputBuffer ${flags}")
-    }
-
-
-    private fun cobbleTogetherACodec(surface : Surface): MediaCodec {
-
-        logInput("cobbleTogetherACodec")
-
-        //val decoder = MediaCodec.createDecoderByType(MediaFormat.MIMETYPE_VIDEO_AVC)
-        logInput("CREATED")
-        val format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, 192, 108)
-        format.setLong(MediaFormat.KEY_DURATION, 96000)
-        format.setInteger(MediaFormat.KEY_CAPTURE_RATE, 25)
-        format.setInteger(MediaFormat.KEY_PROFILE, MediaCodecInfo.CodecProfileLevel.AVCProfileConstrainedBaseline)
-        format.setInteger(MediaFormat.KEY_LEVEL, MediaCodecInfo.CodecProfileLevel.AV1Level21)
-        format.setInteger(MediaFormat.KEY_FRAME_RATE, 25)
-        format.setInteger(MediaFormat.KEY_WIDTH, 192)
-        format.setInteger(MediaFormat.KEY_HEIGHT, 108)
-        format.setInteger(MediaFormat.KEY_MAX_WIDTH, 1920)
-        format.setInteger(MediaFormat.KEY_MAX_HEIGHT, 1080)
-        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, COLOR_FormatYUV420Flexible)
-        val decoder = MediaCodec.createByCodecName( MediaCodecList(MediaCodecList.REGULAR_CODECS).findDecoderForFormat(format) )
-
-        //format.setString(KEY_CODECS_STRING,"avc3.42C015") //api 30
-        //need to work out the format proper like
-        decoder.configure(format, surface, null, 0)
-
-        logInput("CONFIGURED")
-        decoder.start()
-        logInput("STARTED")
-
-        SystemClock.sleep(1000)
-        return decoder
-    }
 }
